0fcb0339d5ac137e2d92f76bbb60a48f1e0d46bd
noddycheung <118799146+noddycheung@users.noreply.github.com>
2023-10-07 10:59:26 -0700

    Add files via upload

--- Plant Wall Design Project Noddy/lib/soilSensor/soilSensor.cpp
+++ Plant Wall Design Project Noddy/lib/soilSensor/soilSensor.cpp
@@ -1,1 +1,21 @@
 #include "soilSensor.h"
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+
+SoilSensor::SoilSensor(int rxPin, int txPin) : _RS485Serial(rxPin, txPin) {
+  _RS485Serial.begin(9600);
+}
+
+float SoilSensor::readTemperature() {
+  // Send request to sensor
+  // Read response
+  // Convert response to temperature
+  // Return temperature
+}
+
+float SoilSensor::readMoisture() {
+  // Send request to sensor
+  // Read response
+  // Convert response to moisture level
+  // Return moisture level
+}

286e8d0a6a39b2a5aef1e6f10087f388abeec7cb
Azurix <61982711+Ecryptic-exe@users.noreply.github.com>
2023-11-24 11:28:28 +0800

    Add files via upload

--- Plant Wall Design Project/lib/flowSensor/FlowSensor.cpp
+++ Plant Wall Design Project/lib/flowSensor/FlowSensor.cpp
@@ -15,47 +15,33 @@ void FlowSensor::begin() {
   attachInterrupt(digitalPinToInterrupt(sensorPin), FlowSensor::increase, RISING); // attach the interrupt handler
 }
 
-bool FlowSensor::getflowRate() {
-  if (totalMilliLitres < TargetValue) // 30mL target value
-    {
-      if(millis() - lastTime <= 1000){
+void FlowSensor::getflowRate() {
+    if(millis() - lastTime >= 1000){
+    // update the flowRate every second
+    // Serial.println("valve high");
+    flowRate = (pulse * 1000 / 60 / 11); // (Pulse frequency x 1000 mL) / 60 min / 11Q = flowrate in mL/s
+    //flowRate = (pulse * 60 / 11); // (Pulse frequency x 60 min) / 11Q = flowrate in L/hour
+    Serial.print("Curmulative water flow:  ");
+    Serial.print(flowRate);
+    Serial.println(" mL/s   ");
+    //Serial.print(" L/h    ");
+    lastTime = millis(); // update the last time
 
-      // update the flowRate every second
-      Serial.println("valve high");
-      //Valve1.valveOpen();
-
-      flowRate = (pulse * 1000 / 60 / 11); // (Pulse frequency x 1000 mL) / 60 min / 11Q = flowrate in mL/s
-      //flowRate = (pulse * 60 / 11); // (Pulse frequency x 60 min) / 11Q = flowrate in L/hour
-
-      Serial.print(flowRate);
-      Serial.println(" mL/s   ");
-      //Serial.print(" L/h    ");
-      lastTime = millis(); // update the last time
-
-      //calculate volume
-      totalMilliLitres += flowRate;
-      // Print the cumulative total of litres flowed since starting
-      Serial.print("Output Liquid Quantity: ");        
-      Serial.print(totalMilliLitres);
-      Serial.println("mL");     
-
-      pulse = 0; // reset the pulse
-      
-      return false;
-      }
+    //calculate volume
+    totalMilliLitres += flowRate;
+    // Print the cumulative total of litres flowed since starting
+    // return totalMilliLitres;
+    pulse = 0; // reset the pulse
     }
-  else if (totalMilliLitres >= TargetValue) // 30mL
-  {
-    Serial.println("valve low");
-    return true;
-    // reset the value and timer
-    lastTime = 0;
-    flowRate = 0;
-    totalMilliLitres = 0;
-    pulse = 0;      
   }
-}
 
 void FlowSensor::increase() {
   pulse++; // increment the pulse
+}
+
+void FlowSensor::reset(){
+    lastTime = 0;
+    flowRate = 0;
+    totalMilliLitres = 0;
+    pulse = 0;    
 }
--- Plant Wall Design Project/src/main.cpp
+++ Plant Wall Design Project/src/main.cpp
@@ -30,17 +30,19 @@ void pinSetup(){
 
 
 void valveInit(){
-  Valve1.initializeValve();
+  // Valve1.initializeValve();
   // Valve2.initializeValve();
   // Valve3.initializeValve();
 }
 
 void testValve(){
-  //Valve1.valveClose();
+  // digitalWrite(solenoidValve1, HIGH);
+  // Valve1.valveClose();
   // Valve2.valveClose();
   // Valve3.valveClose();
   delay(3000);
-  //Valve1.valveOpen();
+  // digitalWrite(solenoidValve1, LOW);
+  // Valve1.valveOpen();
   // Valve2.valveOpen();
   // Valve3.valveOpen();
   delay(3000);
@@ -50,7 +52,7 @@ void testValve(){
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600); // NPK communicates at 9600 bps
-  //soilSensor.begin(); // Initialize the communication with the soil sensor
+  // soilSensor.begin(); // Initialize the communication with the soil sensor
   flowSensor.begin();
   pinSetup();
 
@@ -67,44 +69,49 @@ void setup() {
 
 void loop() {
   //soilSensor
-  // byte temp[] = {0x01,0x03,0x00,0x12,0x00,0x02,0x64,0x0e}; //temp
-  // byte receivedData[9];
+  byte temp[] = {0x01,0x03,0x00,0x12,0x00,0x02,0x64,0x0e}; //temp
+  byte receivedData[9];
 
-  // Serial2.write(temp, sizeof(temp));  // Send the query data to the NPK sensor
-  // delay(1000);  // Wait for 1 second
+  Serial2.write(temp, sizeof(temp));  // Send the query data to the NPK sensor
+  delay(1000);  // Wait for 1 second
 
-  // Serial2.readBytes(receivedData, sizeof(receivedData));  // Read the received data into the receivedData array
+  Serial2.readBytes(receivedData, sizeof(receivedData));  // Read the received data into the receivedData array
 
-  // // Parse and print the received data in decimal format
-  // unsigned int soilTemperature = (receivedData[5] << 8) | receivedData[6];
-  // unsigned int soilHumidity = (receivedData[3] << 8) | receivedData[4];
+  // Parse and print the received data in decimal format
+  unsigned int soilTemperature = (receivedData[5] << 8) | receivedData[6];
+  unsigned int soilHumidity = (receivedData[3] << 8) | receivedData[4];
 
-  // Serial.print("Soil Temperature: ");
-  // Serial.println((float)soilTemperature / 10.0);
-  // Serial.print("Soil Humidity: ");
-  // Serial.println((float)soilHumidity / 10.0);
+  Serial.print("Soil Temperature: ");
+  Serial.println((float)soilTemperature / 10.0);
+  Serial.print("Soil Humidity: ");
+  Serial.println((float)soilHumidity / 10.0);
 
-  // //Soil Humidity Contrl Pump
-  // if (soilHumidity < 30) {
-  //   pump.pumpRate(90);
-  // }
-  // else {
-  //   pump.pumpRate(0);
-  // }
+  //Soil Humidity Contrl Pump
+  if (soilHumidity < 30) {
+    pump.pumpRate(90);
 
-  // conductivity(); // Call the conductivity function
-  // pH(); // Call the pH function
-  // nitrogen(); // Call the nitrogen function
+    // Get Flowrate control Valve
+    flowSensor.getflowRate();
+    if (flowSensor.totalMilliLitres < flowSensor.TargetValue) {
+      Valve1.valveOpen();
+      // Serial.println("Debug");
+      }
+    else if(flowSensor.totalMilliLitres >= flowSensor.TargetValue) {
+        Valve1.valveClose();
+        // Serial.println("Close Debug");
+        pump.pumpRate(0);
+        flowSensor.reset();
+      }
+  }
+  else {
+    pump.pumpRate(0);
+  }
 
-  // Get Flowrate control Valve
-  flowSensor.getflowRate();
-  //pump.pumpRate(90);
-  if (flowSensor.getflowRate() == true) {
-    Valve1.valveOpen();}
-  else if(flowSensor.getflowRate() == false) {
-      Valve1.valveClose();
-      pump.pumpRate(0);
-    }
+  conductivity(); // Call the conductivity function
+  pH(); // Call the pH function
+  nitrogen(); // Call the nitrogen function
+
+
 
   //ultrasonic
   // waterLevelSensor.update();
